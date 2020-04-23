ce9e53c6b6466ca19348ce17fd54084d32a3669e
f14-tomcat <rezakhakpoorboraz@gmail.com>
2020-04-22 23:21:51 -0700

    first commit

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,2 +1,7 @@
 import cv2
-cv2.imread('images/ma_photo.jpeg')
+img = cv2.imread('images/ma_photo.jpeg')
+img2 = cv2.medianBlur(img,5)
+cv2.imshow('ma_photo',img)
+cv2.waitKey(0)
+cv2.imshow('ma_photo',img2)
+cv2.waitKey(0)
